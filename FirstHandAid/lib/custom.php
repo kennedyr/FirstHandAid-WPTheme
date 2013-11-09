9e9ffddb49ec1a041318db60e551d6fe85359825
andrew-cql <andrew.webster@cqlcorp.com>
2013-11-09 15:54:47 -0500

    added header hook

--- FirstHandAid/lib/custom.php
+++ FirstHandAid/lib/custom.php
@@ -5,6 +5,12 @@
 function my_custom_footer_override() {
     get_template_part('templates/fha-footer');
 }
+
+function my_custom_header_override(){
+    get_template_part('templates/fha-header');
+}
+
 add_action( 'shoestrap_footer_override', 'my_custom_footer_override' );
+add_action( 'shoestrap_page_header_override','my_custom_header_override');
 
 ?>
---
+++ FirstHandAid/templates/fha-header.php
@@ -0,0 +1,7 @@
+<header>
+    <h1>BARRRRRRRRR!</h1>
+    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+</header>
+<?php   
+wp_header();
+?>

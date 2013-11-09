90aeb8a228836c53ea144228586cc157889b28ed
Eric Kennedy <ekennedy50@gmail.com>
2013-11-09 16:10:54 -0500

    merge

--- FirstHandAid/lib/custom.php
+++ FirstHandAid/lib/custom.php
@@ -2,15 +2,20 @@
 /**
  * Custom functions
  */
-function my_custom_footer_override() {
+function fha_footer_override() {
     get_template_part('templates/fha-footer');
 }
 
-function my_custom_header_override(){
+function fha_header_override(){
     get_template_part('templates/fha-header');
 }
+function fha_scripts() {
+    wp_enqueue_style( 'fha', get_stylesheet_directory_uri() . '/fha.css' );
+    wp_enqueue_style('shoestrap_css', shoestrap_css( 'url' ), false, null);
+}
 
-add_action( 'shoestrap_footer_override', 'my_custom_footer_override' );
-add_action( 'shoestrap_page_header_override','my_custom_header_override');
+add_action('wp_enqueue_scripts', 'fha_scripts', 100);
+add_action( 'shoestrap_footer_override', 'fha_footer_override' );
+add_action( 'shoestrap_page_header_override','fha_header_override');
 
 ?>

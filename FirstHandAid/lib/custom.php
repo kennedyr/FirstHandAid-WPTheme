78e44e82531e4f7fcae39cc9eac12cbe381f72e5
Eric Kennedy <ekennedy50@gmail.com>
2013-11-09 16:06:48 -0500

    Adding Custom fha.css file

--- FirstHandAid/lib/custom.php
+++ FirstHandAid/lib/custom.php
@@ -2,7 +2,13 @@
 /**
  * Custom functions
  */
-function my_custom_footer_override() {
+function fha_footer_override() {
     get_template_part('templates/fha-footer');
 }
-add_action( 'shoestrap_footer_override', 'my_custom_footer_override' );
+add_action( 'shoestrap_footer_override', 'fha_footer_override' );
+
+function fha_scripts() {
+    wp_enqueue_style( 'fha', get_stylesheet_directory_uri() . '/fha.css' );
+    wp_enqueue_style('shoestrap_css', shoestrap_css( 'url' ), false, null);
+}
+add_action('wp_enqueue_scripts', 'fha_scripts', 100);

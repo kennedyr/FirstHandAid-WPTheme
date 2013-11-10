e514670cdd13e41450dee35da9eddbc6b2d91e2e
Eric Kennedy <ekennedy50@gmail.com>
2013-11-09 20:45:47 -0500

    adding additional html comp assets

--- FirstHandAid/assets/js/fha.js
+++ FirstHandAid/assets/js/fha.js
@@ -1,8 +1,39 @@
 $(window).scroll(function() {
 	
-if(window.pageYOffset >= 768) {
-	$('#head').animate({top: '0'}, 500);
+if(window.pageYOffset >= 668) {
+	$('#headDrop').slideDown();
+} else {
+	$('#headDrop').slideUp();
+}
+
+if(window.pageYOffset >= 1068) {
+	$('.magix-sideNav').fadeIn();
+} else {
+	$('.magix-sideNav').fadeOut();
+}
+
+if(window.pageYOffset < 1600) {
+	resetLinks('whoLink');
+}
+
+if(window.pageYOffset >= 1600) {
+	resetLinks('boardLink');
+}
+
+if(window.pageYOffset >= 2200) {
+	resetLinks('eventLink');
+}
+
+if(window.pageYOffset >= 2800) {
+	resetLinks('pressLink');
 }
 	
-	
 });
+
+
+function resetLinks(type) {
+	$('.hiddenNav').each(function(){
+		$(this).removeClass('selected');
+		$('#' + type).addClass('selected');
+	});
+}
